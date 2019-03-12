d4270eb080c5793ff8b19e46496d35c873d99836
Joe Tustin <j.tustin@gmail.com>
2019-03-12 20:05:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -25,10 +25,12 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
+  return array.shift
 
 end
 
 def retrieve_element_from_index(array, index_number)
+  return array[index_number]
 
 end
 
